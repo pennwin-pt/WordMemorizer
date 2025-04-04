c30b96a4c1800ee32fc139323f3f4b099331c220
PC <penn.wang@xiaonuan.wang>
2025-04-04 18:25:08 +0100

    Apply skin into use by Main Form

--- WordMemorizer.Core/WordMemorizer.Core/FormMain.cs
+++ WordMemorizer.Core/WordMemorizer.Core/FormMain.cs
@@ -15,6 +15,14 @@ namespace WordMemorizer.Core
         public FormMain()
         {
             InitializeComponent();
+            Sunisoft.IrisSkin.SkinEngine skin = new Sunisoft.IrisSkin.SkinEngine();
+            // "Warm.ssk";//"MacOS.ssk";//"XPSilver.ssk";// "EmeraldColor1.ssk";//"MidsummerColor3.ssk";
+            // Emerald /DiamondBlue/ Page
+            // Calmness  /  SteelBlack
+            string skinName = "SteelBlack";
+            skin.SkinFile = System.Environment.CurrentDirectory + "\\skins\\" + skinName + ".ssk";
+            skin.Active = true;
+            StartPosition = FormStartPosition.CenterScreen;
         }
 
         private void FormMain_Load(object sender, EventArgs e)
